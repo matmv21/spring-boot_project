ebc700b3049c93cf82b69b352bed12f1885bfc96
Mateus Vieira <mateusmvsud@hotmail.com>
2024-12-03 15:43:59 -0300

    adjustments made in the User and UserController Classes

--- src/main/java/com/mattmv/course/resources/UserResource.java
+++ src/main/java/com/mattmv/course/resources/UserResource.java
@@ -12,7 +12,7 @@ public class UserResource {
 
     @GetMapping
     public ResponseEntity<User> findAll() {
-        User u = new User(1, "Maria", "maria@gmail.com", "999999999", "12345");
+        User u = new User(1L, "Maria", "maria@gmail.com", "999999999", "12345");
         return ResponseEntity.ok().body(u);
     }
 
