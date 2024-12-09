7856b636732ee582ed83ecb857d6573dadacef3b
Mateus Vieira <mateusmvsud@hotmail.com>
2024-12-09 12:11:12 -0300

    Exception handling - findById

--- src/main/java/com/mattmv/course/services/UserService.java
+++ src/main/java/com/mattmv/course/services/UserService.java
@@ -2,6 +2,8 @@ package com.mattmv.course.services;
 
 import com.mattmv.course.entities.User;
 import com.mattmv.course.repositories.UserRepository;
+import com.mattmv.course.services.exceptions.ResourceNotFoundException;
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +22,7 @@ public class UserService { // bussiness rules engine (BRE)
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
